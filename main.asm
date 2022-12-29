b7f0488a24668d0a1837e2d45a2f52f7fd7f1cdd
Crypt00o <octopus2672001@gmail.com>
2022-12-29 23:22:28 +0200

    add ascii_to_decimal,not_valid_number,scan_char , scan_point  func

--- main.asm
+++ main.asm
@@ -4,7 +4,7 @@ start:
 
 push bp
 mov bp,sp
-sub sp,80h
+sub sp,20h
 
 jmp main
 
@@ -18,7 +18,7 @@ ret
 
 
 print:
-mov ah,9h
+mov ah,09h
 int 21h 
 ret
 
@@ -28,7 +28,52 @@ xor ax,ax
 mov ah,4ch
 int 21h        
 ret
- 
- 
+
+not_valid_number:
+; pushing ascii value "[-] not valid number " into stack
+push 2424h
+push 7265h
+push 626dh
+push 754eh
+push 2064h
+push 696ch
+push 6156h
+push 2074h
+push 6f4eh
+push 205dh
+push 2d5bh
+mov dx,sp
+call print
+call exit 
+ret 
+
+ascii_to_decimal:
+cmp al,30h
+jl not_valid_number
+cmp al,39h
+jg not_valid_number
+sub al,30h
+ret
+
+scan_char:
+mov ah,01h
+int 21h
+ret
+
+scan_point:
+
+call scan_char
+call ascii_to_decimal 
+mov  ptr [di][0],ax ; store x 
+
+call scan_char
+call ascii_to_decimal
+mov ptr [di][1],ax  ; store y
+xor ax,ax
+ret
+
 main:
+
 call exit
+
+ret
